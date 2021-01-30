de51adc4efc38e5f5838af9d628071cbd8860d18
lasupernova <gabrilasuno@gmail.com>
2021-01-30 18:10:41 +0100

    added posting functionality

--- Frames/chat.py
+++ Frames/chat.py
@@ -1,5 +1,6 @@
 # ----- Imports -----
 
+import tkinter as tk
 from tkinter import ttk
 import requests
 import os
@@ -32,6 +33,9 @@ class Chat(ttk.Frame):
         self.message_window = MessageWindow(self) #class creates a Canvas harboring a "scrollable" Frame-object
         input_frame =ttk.Frame(self, padding=10)
 
+        self.message_input = tk.Text(input_frame, height=4) #height in rows of text
+        self.message_input.pack(expand=True, fill="both", side="left", padx=(0,10))
+
         # place frames
         self.message_window .grid(row=0, column=0, sticky="NSEW", pady=10)
         input_frame.grid(row=1, column=0, sticky="EW")
@@ -40,11 +44,12 @@ class Chat(ttk.Frame):
 
         # initiate buttons
         fetch_message = ttk.Button(input_frame, text="Fetch", command=self.get_messages)
+        submit_message = ttk.Button(input_frame, text="Send", command=self.post_message)
 
         # place buttons
         fetch_message.pack() #NOTE: .pack() and .grid() can be used together in the same application but not within the same frame
                              #NOTE: use .pack() for all other objects within input_frame
-
+        submit_message.pack()
         # # uncomment for troubleshooting
         # print(self.winfo_children()) 
          
@@ -57,3 +62,11 @@ class Chat(ttk.Frame):
         # get messages from API and convert reply to JSON
         messages = requests.get("http://167.99.63.70/messages").json()
         self.message_window.message_update_widget(messages, message_labels)
+        self.after(150, lambda: self.message_window.yview_moveto(1.0)) #after 150ms scroll all the way to the bottom
+
+    # method to post messages
+    def post_message(self):
+        text = self.message_input.get("1.0","end").strip()
+        requests.post("http://167.99.63.70/messages", json={"message": text})
+        self.message_input.delete("1.0", "end") #deletes text field content after sending
+        self.get_messages() 
